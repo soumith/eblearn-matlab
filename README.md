7d3d1f55ea366831c04d16e2334cc8cae03f9f58
Soumith Chintala <soumith@gmail.com>
2012-05-23 18:45:15 -0400

    changed the example to reflect usage with eblearn trunk

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Dependencies
 libmatio-dev 
 URL: http://sourceforge.net/projects/matio/ 
 Ubuntu: sudo apt-get install libmatio-dev
+
+Usage
+-----
+> add the line 
+#define __MATLAB__ 
+before including eblearn headers
+
+> Link your program with libmatio (compiler flag -lmatio)
+
+Check main.cpp for sample usage and CMakelists.txt for sample linking
--- main.cpp
+++ main.cpp
@@ -1,47 +1,21 @@
 #include <iostream>
-#include "ebl_matwrapper.h"
-#include <eblearn/libidx.h>
-#include <matio.h>
+#define __MATLAB__ 
+#include <eblearn/libidx.h> 
 
-int main(int argc, char **argv) {
-    
-    const char *matFileName = "testMATLAB.mat";
-    const char* varName = "var_a";
-     ebl::matlab matWrapper(matFileName);
-     ebl::idx<double> varToGet=matWrapper.load_matrix<double>(varName);
-     for(int i=0;i<10;i++) {
-	for(int j=0;j<10;j++) {
-	     cout<<varToGet.get(i,j)<<" ";
-	}
-	cout<<endl;
-}
-cout<<"Tensor dimensions"<<varToGet.order()<<endl;
-     int err = 0;
-     mat_t *mat;
-     matvar_t *matvar;
-cout<<"------------------------------------------"<<endl;
-     mat = Mat_Open(matFileName,MAT_ACC_RDONLY);
-     if ( mat ) {
-         matvar = Mat_VarRead(mat,varName);
-         if ( matvar == NULL ) {
-             err = 1;
- 	    cout<<"no var"<<endl;
-         } else {
-		for ( int i = 0; i  <10 ; i++ ) {
-     for ( int j = 0; j <10; j++ ) {
-             printf("%f ", ((double*)matvar->data)[matvar->dims[0]*j+i]);
-}
-                 printf("\n");
- }
-
-             Mat_VarFree(matvar);
-         }
-         Mat_Close(mat);
-     } else {
-         err = 1;
-     }
-
-
-    
-    return 0;
+int main(int argc, char **argv) {    
+  char *matFileName = "testMATLAB.mat";
+  char* varName = "var_a";
+  cout<< "Reading variable: \"" << varName 
+      << "\" from file: " << matFileName <<endl;
+  // create ebl::matlab object
+  ebl::matlab matWrapper(matFileName);
+  // read the variable into an idx
+  ebl::idx<double> varToGet=matWrapper.load_matrix<double>(varName);
+  // Display tensor information
+  varToGet.pretty();
+  cout << endl;
+  // display the idx values
+  varToGet.printElems();
+  cout << endl;
+  return 0;
 }
